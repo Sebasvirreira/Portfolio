0174935152a48e75b509f06158f0ed47d95d45f6
Sebasvirreira <sebasvirreira@gmail.com>
2024-10-16 12:46:39 -0400

    new changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import logo from "../assets/kevinRushLogo.png";
+import logo from "../assets/SVLogo.png";
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { FaSquareXTwitter } from "react-icons/fa6";
@@ -11,10 +11,12 @@ const Navbar = () => {
         <img className="mx-2 w-10" src={logo} alt="logo" />
       </div>
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-        <FaLinkedin />
-        <FaGithub />
-        <FaSquareXTwitter />
-        <FaInstagram />
+        <a href="">
+          <FaLinkedin />
+        </a>
+        <a href="">
+          <FaGithub />
+        </a>
       </div>
     </nav>
   );
